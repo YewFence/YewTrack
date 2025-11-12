ab77a96681d76a2a1eb837f985ffd82059916782
YewFence <88962785+YewFence@users.noreply.github.com>
2025-11-12 09:34:45 +0800

    fix: 用tailwindcss3

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,9 @@
-// tailwind.config.js
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}", // 关键：在这里告诉 Tailwind 要扫描哪些文件
-  ],
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
